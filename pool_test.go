21d45f0aa8c7a2c179b37a7d79e13e460d8f25c7
Boris Varbanov <boris@codechimp.io>
2016-06-25 17:07:31 +0100

    Update .travis.yml

--- pool_test.go
+++ pool_test.go
@@ -8,8 +8,8 @@ import (
 func Test_NewPool(t *testing.T) {
 	pool := New(100, 1000)
 
-	iterations := 1000000
-	var counter uint64 = 0
+	iterations := 100000
+	var counter uint64
 
 	for i := 0; i < iterations; i++ {
 		arg := uint64(1)
@@ -17,17 +17,17 @@ func Test_NewPool(t *testing.T) {
 		job := func() {
 			atomic.AddUint64(&counter, arg)
 			if arg != uint64(1) {
-    			t.Errorf("Expected: %d, got: %d", uint64(1), arg)
+				t.Errorf("Expected: %d, got: %d", uint64(1), arg)
 			}
 		}
 
 		pool.JobQueue <- job
 	}
 
-    pool.Stop()
+	pool.Stop()
 
 	counterFinal := atomic.LoadUint64(&counter)
-	if counterFinal != uint64(iterations){
-    	t.Errorf("Expected: %d, got: %d", uint64(iterations), counterFinal)
+	if counterFinal != uint64(iterations) {
+		t.Errorf("Expected: %d, got: %d", uint64(iterations), counterFinal)
 	}
 }
