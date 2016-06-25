6de8c172222880a69f269be017127fb08e0d1f23
Boris Varbanov <boris@codechimp.io>
2016-06-25 20:23:34 +0100

    Rename the example

--- _example/simplepool.go
+++ _example/simplepool.go
@@ -1,3 +1,4 @@
+// +build ignore
 package main
 
 import (
@@ -8,17 +9,17 @@ import (
 
 func main() {
 	// Number of workers, and Size of the job queue
-	pool := workerpool.New(10, 50)
+	simplepool := workerpool.New(10, 50)
 
 	// create and submit 100 jobs to the pool
 	for i := 0; i < 100; i++ {
 		count := i
 
-		pool.JobQueue <- func() {
+		simplepool.JobQueue <- func() {
 			fmt.Printf("I am job! Number %d\n", count)
 		}
 	}
 
 	// Wait for all jobs to finish and stop the workers
-	pool.Stop()
+	simplepool.Stop()
 }
